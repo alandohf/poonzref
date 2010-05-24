4f285ce18be296c51ce1f5826d30884db3953b7b
pzw.notes <pzw.notes@c30a8875-6c86-0be0-f4a7-9e2ae0c050c1>
2010-05-24 15:36:45 +0000

    myc.c myc.h

---
+++ dev/gcc/myh.h
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  bubbleSort.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  05/21/2010 08:58:50 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+/* 
+ * self-defined type 
+ */
+typedef  int TwoElementArray[8];
+typedef  char C_TwoElementArray[8];
+
+int 	bubbleSort(int a[], int array_size);
+int     printArray( int array[], int array_size);
