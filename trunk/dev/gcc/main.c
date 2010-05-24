1033fc178ecdb45358c2f8be0555c316c95c9124
pzw.notes <pzw.notes@c30a8875-6c86-0be0-f4a7-9e2ae0c050c1>
2010-05-24 15:35:35 +0000

    add myc.c myc.h

--- trunk/dev/gcc/main.c
+++ trunk/dev/gcc/main.c
@@ -1,16 +1,34 @@
 #include	<stdio.h>
 //#include "display.h"
 //#include "prices.h"
-#include "./bubbleSort.h"
+//#include "bubbleSort.h"
+#include	"myh.h"
 
 int
 main()
 {
-//  display_options();
-//  calculate_price();
-//  display_price();
+
+/*  
+ * // test display();
+  display_options();
+  calculate_price();
+  display_price();
+*/
+
+/*  
+ * // test  bubbleSort
 int array[10] = {10,9,8,7,6,5,4,3,2,1};
     bubbleSort( array, 10);
     printArray( array, 10);
+*/
+//test array type
+    TwoElementArray array={1,2,3,4,5,6,7,8};
+    TwoElementArray array={1,2,3,4,5,6,7,8};
+//    C_TwoElementArray array={'1','2','3','4','5','6','7','8'};
+//    array = { 0, 9 };
+
+    printArray( array, 8);
+
+
   return 0;
 }
