4cccce61530032b8790a4600670ed15e2ac01e97
pzw.notes <pzw.notes@c30a8875-6c86-0be0-f4a7-9e2ae0c050c1>
2010-06-13 00:14:03 +0000

    grant execute & create synonyms

---
+++ trunk/dev/plsql/admin.sql
@@ -0,0 +1,11 @@
+--usage:grant execute on package/function/procedure to user；
+exec pdict('DBA_'); --fail
+--exec sys.pdict('DBA_');
+--revoke execute on pdict from pzw;
+grant execute on pdict to pzw;
+drop public synonym pdict;
+create public synonym pdict for sys.pdict;
+exec pdict('DBA_');
+--pub_emp属于public用户，数据库所有用户都可以访问。
+drop public synonym r_date;
+create public synonym r_date for pzw.r_date;
---
+++ trunk/dev/plsql/readme.txt
@@ -0,0 +1,5 @@
+fdict	a function to get description of tables
+pdict	a procedure to get description of tables
+mega 	a function to calculate bytes into mega bytes
+giga 	a function to calculate bytes into giga bytes
+
