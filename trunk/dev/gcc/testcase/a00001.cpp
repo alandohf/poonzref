f3478f4fc80af7449ae63d5ad0abee3f600d6eb5
pzw.notes@gmail.com <pzw.notes@gmail.com@c30a8875-6c86-0be0-f4a7-9e2ae0c050c1>
2012-03-12 02:18:55 +0000

    example: test hello world in cpp

--- trunk/dev/gcc/testcase/a00001.cpp
+++ trunk/dev/gcc/testcase/a00001.cpp
@@ -1,26 +1,12 @@
 /**
-*按行读取文件内容，文件缓冲区长度固定
+* example: test hello world in cpp
+*
 **/
+#include <iostream>
+using namespace std;
 
-#include <stdio.h>
-#include <string.h>
-
-int main()
-{
-    char str[200];
-    FILE *fp;
-
-    fp = fopen("t00101.c", "r");
-    if(!fp) return 1; // bail out if file not found
-    while(fgets(str,sizeof(str),fp) != NULL)
-    {
-        // strip trailing '\n' if it exists
-        int len = strlen(str)-1;
-        if(str[len] == '\n')
-            str[len] = 0;
-        printf("\n%s", str);
-    }
-    fclose(fp);
-	return 0;
+int 
+main(){
+	cout<<"hello world!"<<endl;
+return 0;	
 }
-
