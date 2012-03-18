8547e5120b438fc26e36e5ab3531cf5733d4c985
pzw.notes@gmail.com <pzw.notes@gmail.com@c30a8875-6c86-0be0-f4a7-9e2ae0c050c1>
2012-03-18 17:32:00 +0000

    1.基于对话框的编程； 2.改了makefile的命名格式

---
+++ dev/myworks/win32/resource.h
@@ -0,0 +1,28 @@
+//~ t00004_MenuWithResponse.rc
+
+//~ #define IDR_MENU1  9001
+#define IDM_ABOUT1 9002
+
+//~ t00005_BasicDlgBaseApp.rc
+#define IDD_DIALOG1                     101
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
+
+
+//~ t00006_DlgBaseAppWithMenu.c
+#define IDC_BUTTON1                     1002
+#define IDR_MENU1                       108
+#define ID_MENUITEM40001                40001
+#define ID_MENUITEM40002                40002
+#define ID_MENUITEM40003                40003
+#define ID_MENUITEM40005                40005
+
