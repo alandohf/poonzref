c4517e99e92f2f63dcb2850b2b0765e6d52481fc
pzw.notes@gmail.com <pzw.notes@gmail.com@c30a8875-6c86-0be0-f4a7-9e2ae0c050c1>
2012-03-21 15:26:06 +0000

    stack v0.1

--- trunk/dev/gcc/testcase/c_snippets/stack/stack.c
+++ trunk/dev/gcc/testcase/c_snippets/stack/stack.c
@@ -1,9 +1,21 @@
+//http://blog.csdn.net/astropeak/article/details/6653048
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include "stack.h"
+#include "poonapi.h"
 
-int InitStack(){
-	PNODE
-	return 0;
+pArrSTACK InitStack(int size){
+	
+	pArrSTACK pS = (pArrSTACK) malloc(sizeof(size*STACK_INIT_SIZE));
+	if(NULL == pS) exitm("not enough memory!");
+	memset(pS,0,sizeof(size*STACK_INIT_SIZE));
+	pS->actSize = 0;
+	return pS;
 }
+
+int isEmpty(pArrSTACK s){
+	return (s->actSize == 0 ) ? TRUE : FALSE;
+}
+
